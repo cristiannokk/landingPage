caa62d274daa51938614b6fce396bad53f038580
Cristian Fernandez <83661346+cristiannokk@users.noreply.github.com>
2024-07-23 17:42:39 -0300

    prueba

--- app/(default)/page.tsx
+++ app/(default)/page.tsx
@@ -14,7 +14,7 @@ export default function Home() {
   return (
     <>
       <div className="relative">
-        <LottieBackground /> {/* Agrega el componente LottieBackground */}
+        {/* <LottieBackground /> Agrega el componente LottieBackground */}
         <div className="relative z-10"> {/* Asegúrate de que el contenido principal esté en un contenedor con z-index más alto */}
           <Hero />
           <BusinessCategories />
