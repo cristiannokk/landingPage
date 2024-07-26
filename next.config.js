31717481f7562856c65a78be69735296f4c7df23
Cristian Fernandez <83661346+cristiannokk@users.noreply.github.com>
2024-07-26 14:51:18 -0300

    .

--- next.config.js
+++ next.config.js
@@ -1,4 +1,15 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  productionBrowserSourceMaps: true,
+  reactStrictMode: false,
+  transpilePackages: ['antd'],
+  swcMinify: true,
+  compiler: {
+    emotion: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
 
 module.exports = nextConfig;
