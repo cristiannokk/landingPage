cae61a27827ed01caa21c639df3396ff5422b20e
cristian <cristiannokk@gmail.com>
2024-08-07 19:07:07 -0300

    ...

--- next.config.js
+++ next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  eslint: {
+    // Completely disable ESLint
+    ignoreDuringBuilds: true,
+  },
+}
 
 module.exports = nextConfig
